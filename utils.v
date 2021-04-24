fa12c728db7822bbc03dbd8bf8cacdd6a9a8e8c2
Saverio <personnex976@gamil.com>
2021-04-24 22:42:44 +0200

    fix : when data_to_strip is less than 9 char strike error

--- utils.v
+++ utils.v
@@ -6,7 +6,8 @@ fn strip_tag(tag string, data string) string {
 	mut to_keep_start := tag.len + 2
 	mut to_keep_end := data.len - to_keep_start - 1
 	mut data_strip := data[to_keep_start..to_keep_end]
-	if data_strip[0..9].to_upper() == '<![CDATA[' {
+
+	if data_strip.len >= 9 && data_strip[0..9].to_upper() == '<![CDATA[' {
 		to_keep_start = 9
 		to_keep_end = data_strip.index(']]></![cdata[') or { panic("what's going on ?") }
 		data_strip = data_strip[to_keep_start..to_keep_end - 1]
