a90f1befe896dd083a2fc6c33c3d6fe822dc271a
Saverio <personnex976@gamil.com>
2021-04-25 20:48:31 +0200

    v fmt

--- examples/basic.v
+++ examples/basic.v
@@ -1,25 +1,25 @@
-/* Choose one of this possibility :*/
+// Choose one of this possibility :
 // if you installed the module in your project folder :
 // uncomment the next line
-//import feedparser
+// import feedparser
 
 // if you installed the module globaly :
 // uncomment the next line
-//import saverio.feedparser
+// import saverio.feedparser
 
 fn main() {
 	url := 'https://rss.nytimes.com/services/xml/rss/nyt/World.xml'
 	mut newsfeed := feedparser.parse(url) or { panic(err.str()) }
 
-	println('Feed\'s title : ${newsfeed.title}')
-	println('Feed\'s link : ${newsfeed.link}')
-	println('Feed\'s description : ${newsfeed.description}')
+	println('Feed\'s title : $newsfeed.title')
+	println('Feed\'s link : $newsfeed.link')
+	println('Feed\'s description : $newsfeed.description')
 
 	// print only 3 results to not spam the terminal :)
 	for entry in newsfeed.entries[..2] {
-		println('\nEntry\'s Title : ${entry.title}')
-		println('Entry\'s Link : ${entry.link}')
-		println('Entry\'s Id : ${entry.id}')
-		println('Entry\'s description : ${entry.description}')
+		println('\nEntry\'s Title : $entry.title')
+		println('Entry\'s Link : $entry.link')
+		println('Entry\'s Id : $entry.id')
+		println('Entry\'s description : $entry.description')
 	}
 }
