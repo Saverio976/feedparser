06a4024c27e4fed11b0e38cbd8f0ab2a39010238
Saverio <personnex976@gamil.com>
2021-04-26 18:42:14 +0200

    adjust with how to init feed struct and entry struct

--- parse_atom_feed.v
+++ parse_atom_feed.v
@@ -2,40 +2,11 @@ module feedparser
 
 import net.html
 
-fn parse_atom_feed(document_dom html.DocumentObjectModel) Feed {
-	newfeed := Feed{
+fn parse_atom_feed(document_dom html.DocumentObjectModel) ?Feed {
+	mut newfeed := Feed{
 		feed_dom: document_dom
-		title: get_feed_title(document_dom) // from parse_common_func.v
-		link: get_feed_link(document_dom) // from parse_common_func.v
-		description: get_feed_description_atom(document_dom)
-		entries: get_entries_atom(document_dom)
 		feed_type: 'atom'
 	}
+	newfeed.initialize_struct() or { return err }
 	return newfeed
-}
-
-fn get_feed_description_atom(document_dom html.DocumentObjectModel) string {
-	mut description := document_dom.get_tag('subtitle')[0].str()
-	description = strip_tag('subtitle', description)
-	return description
-}
-
-fn get_entries_atom(document_dom html.DocumentObjectModel) []Entry {
-	mut entries := []Entry{}
-	mut entry := Entry{}
-	for item in document_dom.get_tag('entry') {
-		entry = Entry{
-			entry_dom: html.parse(item.str())
-		}
-		entry.title = entry.search_tag('title')
-		entry.link = entry.search_tag('link')
-		entry.id = entry.search_tag('id')
-		mut description := entry.search_tag('summary')
-		if description == '' {
-			description = entry.search_tag('content')
-		}
-		entry.description = description
-		entries << entry
-	}
-	return entries
 }
--- parse_rss_feed.v
+++ parse_rss_feed.v
@@ -2,36 +2,11 @@ module feedparser
 
 import net.html
 
-fn parse_rss_feed(document_dom html.DocumentObjectModel) Feed {
-	newfeed := Feed{
+fn parse_rss_feed(document_dom html.DocumentObjectModel) ?Feed {
+	mut newfeed := Feed{
 		feed_dom: document_dom
-		title: get_feed_title(document_dom) // from parse_common_func.v
-		link: get_feed_link(document_dom) // from parse_common_func.v
-		description: get_feed_description_rss(document_dom)
-		entries: get_entries_rss(document_dom)
 		feed_type: 'rss'
 	}
+	newfeed.initialize_struct() or { return err }
 	return newfeed
-}
-
-fn get_feed_description_rss(document_dom html.DocumentObjectModel) string {
-	mut description := document_dom.get_tag('description')[0].str()
-	description = strip_tag('description', description)
-	return description
-}
-
-fn get_entries_rss(document_dom html.DocumentObjectModel) []Entry {
-	mut entries := []Entry{}
-	mut entry := Entry{}
-	for item in document_dom.get_tag('item') {
-		entry = Entry{
-			entry_dom: html.parse(item.str())
-		}
-		entry.title = entry.search_tag('title')
-		entry.link = entry.search_tag('link')
-		entry.id = entry.search_tag('guid')
-		entry.description = entry.search_tag('description')
-		entries << entry
-	}
-	return entries
 }
