3488ddddf2fc28a19fa040046f46b0f7aa8774bd
Saverio <personnex976@gamil.com>
2021-06-03 16:13:46 +0200

    add one test to htmlentity_to_string

--- xmlreplacement/xmlreplacement_test.v
+++ xmlreplacement/xmlreplacement_test.v
@@ -3,4 +3,6 @@ module xmlreplacement
 fn test_htmlentity_to_string() {
 	data := '&amp; &#162;'
 	assert htmlentity_to_string(data) == '& ¢'
+	data1 := 'abcd'
+	assert htmlentity_to_string(data1) == 'abcd'
 }
